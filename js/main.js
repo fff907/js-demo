a00c1bfd10ba35905b80acfdf6b6d1d3ed1053c3
fff907 <59164817+fff907@users.noreply.github.com>
2025-05-12 16:32:26 +0900

    push

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+// クリックイベント
 const btn = document.getElementById("clickBtn");
 const text = document.getElementById("clickText");
 
 btn.addEventListener("click", () => {
     text.textContent = "クリックされました！";
 })
+
+// ループ処理とDOM追加
+const fruits =["りんご", "バナナ", "みかん"];
+const list = document.getElementById("fruitList");
+
+fruits.forEach(fruit => {
+    const li = document.createElement("li");
+    li.textContent = fruit;
+    list.appendChild(li);
+});
